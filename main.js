3d8a8972d1fe284483811ca5d7314730780d20f9
MaverickFR <49167110+M4verickFr@users.noreply.github.com>
2021-02-05 12:03:16 +0100

    removing unnecessary code

--- main.js
+++ main.js
@@ -6,20 +6,7 @@ const fs = require('fs');
 const { exit } = require('process');
 const tools = require('./tools.js')
 
-let queryReviews = fs.readFileSync("./queryReviews.graphql", 'utf8');
-let queryProfil = fs.readFileSync("./queryProfil.graphql", 'utf8');
-
 let currentDate = new Date().getTime()
-
-let url = "https://api.tripadvisor.com/api/internal/1.0/graphql/?currency=EUR&lang=fr_FR"
-
-let headers = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'X-TripAdvisor-API-Key': 'ce957ab2-0385-40f2-a32d-ed80296ff67f',
-    'X-TripAdvisor-UUID': '3635d637-5325-4797-bfec-a30662d92450',
-    'Accept-Encoding': 'gzip'
-};
 
 let args = minimist(process.argv.slice(2), {
     alias: {
