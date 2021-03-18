007e9a2d56cc4ee74b32aa7e4c0e6210e9427c95
Maverick Perrollaz <maverick.perrollaz@pm.me>
2021-03-18 11:42:09 +0100

    add config file for score

--- src/scoreReview.js
+++ src/scoreReview.js
@@ -1,8 +1,13 @@
+const config = require('../config.json');
+
 module.exports = {
     reviewHasPhotos : function(review, profil, location, currentDate) {
-        return 0
+        return review.photos.length > 0 ? config['reviewHasPhotos'] : 0
     },
     helpfulVote : function(review, profil, location, currentDate) {
+        return 0
+    },
+    rateDeviation: function(review, profil, location, currentDate) {
         return 0
     }
 }
